331c70927a6e7ae963f38363c3d805e547b223de
Jin Yoo <jyoo@BrainStations-MacBook-Air-26.local>
2017-02-27 16:40:46 -0500

    removing weather data because of CORS

--- src/components/ControlBar.js
+++ src/components/ControlBar.js
@@ -7,9 +7,8 @@ class ControlBar extends Component {
 					<i className="fa fa-backward control-bar__btn" onClick={this.props.goToPrevStep}></i>
 					<i className={this.props.playing? 'fa fa-pause control-bar__btn' : 'fa fa-play control-bar__btn'} onClick={this.props.togglePlay}></i>
 					<i className="fa fa-forward control-bar__btn" onClick={this.props.goToNextStep}></i>
-					<span onClick={this.props.getRandomData} className="control-bar__btn">USE RANDOM DATA</span>
-					<span onClick={this.props.getWeatherData} className="control-bar__btn">USE TORONTO WEATHER DATA</span>
 					<span onClick={this.props.addRandom} className="control-bar__btn">ADD BAR</span>
+					<span onClick={this.props.getRandomData} className="control-bar__btn">USE NEW SET OF RANDOM DATA</span>				
 			</div>
 		)
 	}
